f1572385648a01e1785ccdf079616d0a2ed6c3fa
Khaled Mahmoud <kmahmou1@david.cs.binghamton.edu>
2018-08-13 20:18:15 -0400

    Modifying run script to accept additional javaagent parameters and to also accept additional class path paths. Class path string must start with :

--- run/run.sh
+++ run/run.sh
@@ -10,13 +10,15 @@ jar_url=file:/$jar_path
 
 rm chappie.*.csv chappie.$jar_name -rf
 
-export CHAPPIE_PATH="/home/timur/Projects/chappie/chappie.jar"
+export CHAPPIE_PATH="/home/kmahmou1/chappie_project/chappie/chappie.jar"
 export JARS="$CHAPPIE_PATH:$jar_path"
 
 extra_jars=$2
 export JARS="$JARS:$extra_jars"
 
-java -Xbootclasspath/p:$CHAPPIE_PATH -cp $JARS -javaagent:$CHAPPIE_PATH chappie.Chaperone $jar_url $3 $4
+##If method_stats.jar agent is be invoked ...  $5 will be the full -javaagent parameter
+##The calling script knows the path of its own chappie installation and the path the the jar file of method_utils.jar
+java -Xbootclasspath/p:$CHAPPIE_PATH:$6 -cp $JARS$6 $5 -javaagent:$CHAPPIE_PATH chappie.Chaperone $jar_url $3 $4
 
 echo 'Moving data to chappie.'${jar_name}
 mkdir chappie.$jar_name
