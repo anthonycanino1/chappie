8ab074f23ce3590e87388c2cfea881010241992c
Anthony Canino <anthony.canino1@gmail.com>
2018-09-05 14:35:54 -0400

    Modified run script to use a relative path. You should no longer need to modify with your own run.sh location. Please do not change and commit the script.
    Furthermore, any settings should be exported outside the script. We
    don't want personal script changes to pollute the repository.

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+dir=`dirname "$0"`
+
 jar_path=$1
 jar_path=$(echo "$(cd "$(dirname "$jar_path")" && pwd)/$(basename "$jar_path")")
 
@@ -9,10 +11,7 @@ jar_name=${jar_name%\.*}
 jar_url=file:/$jar_path
 
 rm chappie.*.csv chappie.$jar_name -rf
-export ITERS=1
-export STACK_PRINT=1
-export MEMORY=1
-export CHAPPIE_PATH="/home/acanino1/Projects/chappie/chappie.jar"
+export CHAPPIE_PATH="$dir/../chappie.jar"
 export JARS="$CHAPPIE_PATH:$jar_path"
 
 extra_jars=$2
