9549c0c6de8859a474345ddc993ec778ec237664
Khaled Zuhair Mahmoud <kmahmou1@binghamton.edu>
2019-01-30 17:44:50 -0500

    Comments explaining what logic will be placed in both attribution functions

--- src/chappie/online/Attribution.java
+++ src/chappie/online/Attribution.java
@@ -33,11 +33,25 @@ public class Attribution {
         int current_epoch = chappie.get_current_epoch();
         List<String> sys_jiffies = chappie.get_sys_jiffies(current_epoch,epochs);
         List<List<Object>> app_jiffies = chappie.application_jiffies(current_epoch,epochs);
+
+        /**
+         * Logic will be performed here is the same as the first part of data_processing.py.
+         * This is already implemented in chappie_atrribution repository
+         * 
+         */
+
+
+
         return osReport;
     }
 
     public static Map<Integer, List<ThreadEnergyAttribution>> get_thread_energy_reports(AppOSActivityReport os_report) {
         HashMap<Integer, List<ThreadEnergyAttribution>> energy_reports = new HashMap<>();
+
+        /**
+         * Logic will be performed here is the same as the second part of data_processing.py.
+         * This will be implemented as part of the chappie_attribtuon project
+         */
         return energy_reports;
     }
 
