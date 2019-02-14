ba6546712d973ef03fda432979c63b13641a924e
Rachit Saxena <rachitsaxena15@gmail.com>
2019-02-14 18:46:19 -0500

    Merge for online attribution

---
+++ src/chappie/config/TraceIndices.java
@@ -0,0 +1,16 @@
+package chappie.config;
+
+public class TraceIndices {
+	
+	/*
+	 * 'epoch', 'time', 'diff', 'socket', 'package', 'dram'
+	 */
+	
+	public static int EPOCH = 0;
+	public static int TIME = 1;
+	public static int DIFF = 2;
+	public static int SOCKET = 3;
+	public static int PACKAGE = 4;
+	public static int DRAM = 5;
+
+}
