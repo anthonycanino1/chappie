816be2925d5bac4e96a90ff188e356563d84ccb4
Khaled Zuhair Mahmoud <kmahmou1@binghamton.edu>
2019-02-14 15:38:51 -0500

    Adding an online tester task

---
+++ src/chappie/online/OnlineTester.java
@@ -0,0 +1,41 @@
+package chappie.online;
+
+import chappie.Chaperone;
+
+import java.util.TimerTask;
+
+public class OnlineTester implements Runnable {
+
+    private Chaperone chappie;
+    private int frequency;
+
+    @Override
+    public void run() {
+        while(true) {
+            int start = chappie.get_current_epoch();
+            try {
+                Thread.sleep(frequency);
+            } catch(Exception exc) {
+                exc.printStackTrace();
+            }
+            int end = chappie.get_current_epoch();
+            Attribution.get_all_thread_attrib(start, end);
+        }
+    }
+
+    public Chaperone getChappie() {
+        return chappie;
+    }
+
+    public void setChappie(Chaperone chappie) {
+        this.chappie = chappie;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}
