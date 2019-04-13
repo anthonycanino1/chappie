ac4328239d567b0cf218b18ec68375e67771e30e
timur <atpoverload@gmail.com>
2019-04-12 21:46:28 -0400

    removed hp build files

--- src/async/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/acanino1/Research/honest-profiler
-# Build directory: /home/acanino1/Research/honest-profiler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unitTests "build/unitTests")
