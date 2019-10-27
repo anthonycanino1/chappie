f851c107a4491753c0911b8b03e3a32d75327420
Timur Ergin Babakol <timur@vaporeon>
2019-10-27 16:41:54 -0400

    quick checkpoint

---
+++ src/chappie/profile/impl/TraceProfiler.java
@@ -0,0 +1,74 @@
+/* ************************************************************************************************
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * Copyright 2019 SUNY Binghamton
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ * ***********************************************************************************************/
+
+package chappie.profile.impl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import asgct.ASGCTReader;
+
+import chappie.Chaperone;
+import chappie.profile.*;
+import chappie.profile.util.*;
+
+public class TraceProfiler extends Profiler {
+  // Since jRAPL returns a double[] from getEnergyStats, I wrote a helper to
+  // reshape it into a socket x 4 array. This primarily helps with clean code
+  // but could (should?) be in jRAPL instead.
+  private static boolean noRapl = false;
+
+  public TraceProfiler(int rate, int time, String workDirectory) {
+    super(rate, time, workDirectory);
+  }
+
+  private static class TraceRecord extends Record {
+    private ASGCTReader.ASGCTFrame frame;
+
+    private TraceRecord(int epoch, ASGCTReader.ASGCTFrame frame) {
+      this.epoch = epoch;
+      this.frame = frame;
+    }
+
+    protected String stringImpl() {
+      String message = frame.getId() + ";" + frame.getTimestamp() + ";";
+      for (String method: frame.getTrace())
+        message += method + "@";
+
+      message = message.substring(0, message.length() - 1);
+
+      return message;
+    }
+
+    private static final String[] header = new String[] { "epoch", "id", "timestamp", "trace" };
+    public static String[] getHeader() {
+      return header;
+    };
+  }
+
+  protected void sampleImpl(int epoch) {
+    if (!noRapl)
+      for (ASGCTReader.ASGCTFrame record: ASGCTReader.fetch())
+        data.add(new TraceRecord(epoch, record));
+  }
+
+  public void dumpImpl() throws IOException {
+    CSV.write(data, TraceRecord.getHeader(), Chaperone.getWorkDirectory() + "/method.csv");
+  }
+}
